a754def7fb52527905d12380a185d0fc3bea095d
Samat Tulegenov <magnit56@gmail.com>
2021-10-29 19:13:40 +0500

    Добавлены типы

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-function distributeSmoothies($m, $n) {
+function distributeSmoothies(int $m, int $n): int {
     return ($m == 0) ? 0 : ($n - $n % $m)/ $m;
 }
 
 $line = trim(fgets(STDIN));
 [$m, $n] = explode(' ', $line);
-print_r(distributeSmoothies($m, $n));
+print_r(distributeSmoothies((int) $m, (int) $n));
